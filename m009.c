1044e8b1f9d87b3afa15e62010bb3788942d39bc
igor matheus bruciere <igorbruciere@usp.br>
2020-07-02 20:59:00 +0000

    ex m009

--- m009.c
+++ m009.c
@@ -23,8 +23,24 @@
 
 /* Sort the first 'n' integers values in 'vector'. */
 
-void sort (int* vector, int n)
+void sort (int *vector, int n)
 {
+  
+  int i = 1, j, x;
+  while (i < n)
+ 
+  {
+    x = vector[i];
+    j = i - 1;
+    while (j >= 0 && vector[j] > x)
+    
+    {
+      vector[j + 1] = vector[j];
+      j = j - 1;}
+   
+    vector[j + 1] = x;
+    i = i + 1;
+  }
 }
 
 #define USAGE "m009 <num1> <nun2> ... \n"
@@ -40,7 +56,6 @@ int main (int argc, char **argv)
   
   for (i=1; i<argc; i++)
     values[i-1] = atoi(argv[i]);
-
 
   /* Sort vector. */
   
